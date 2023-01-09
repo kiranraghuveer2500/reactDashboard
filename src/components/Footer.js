46a54955123261de4ef42c1ebe76d54601bcfeb3
Kiran <kiranraghuveer2500@gmail.com>
2023-01-09 17:14:13 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <input id="button" type="button" value="scan qr code"></input>
+    </footer>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <h1 className="Header">
+      <p>BABA KNOWLEDGE CENTER</p>
+    </h1>
+  );
+}
